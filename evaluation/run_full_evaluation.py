f61c2577b9dffcef5a53bfc53dbae788783bb5e7
Ormin Joseph <orminjoseph@gmail.com>
2026-09-06 20:01:24 +0100

    run_full_evaluation modified

--- evaluation/run_full_evaluation.py
+++ evaluation/run_full_evaluation.py
@@ -15,7 +15,15 @@ Outputs:
 
 Author: Ormin Joseph
 """
+import os
+import sys
 
+PROJECT_ROOT = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
 from evaluation.evaluate_model import evaluate
 
 from evaluation.reconstruction_gallery import (
